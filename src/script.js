0de934feddf757505f5390cacd236405e40ba10f
Christian Rojas <44682470+BlueFits@users.noreply.github.com>
2022-06-21 09:59:15 -0400

    Working screenshot and templating

--- src/script.js
+++ src/script.js
@@ -2,18 +2,33 @@ const Sample = require("./sample.js");
 const PuppeteerBrowser =  require("./PuppeteerBrowser.js");
 const readline = require("readline");
 
-(async () => {
+const fs = require("fs");
+const { TemplateHandler } = require("easy-template-x");
 
-    console.log(Sample);
-    const browserInstance = await PuppeteerBrowser.build();
-    browserInstance.screenshot();
+(async () => {
+    // const browserInstance = await PuppeteerBrowser.build();
+    // browserInstance.screenshot();
+
+    const templateFile = fs.readFileSync(__dirname + '/sample.docx');
+
+    const data = {
+        sample: [
+            { testVal: 'Alon Bar'},
+            { testVal: 'Bitch Bar' }
+        ]
+    };
+
+    const handler = new TemplateHandler();
+    const doc = await handler.process(templateFile, data);
+
+    fs.writeFileSync('myTemplate - output.docx', doc);
 
     const read = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
     });
 
-    read.question(`Press Enter to close... `, input => {
+    read.question(`Press Enter to close... `, (input) => {
         // if (input === "y") {
         //     console.log("yes");
         // } else if (input === "n") {
